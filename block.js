46dd09cc4f85f0ac6e2d3b5621553f6dd43c022b
Marilzon <maxmaril@hotmail.com>
2021-09-15 20:00:16 -0300

    Feature: SHA256 module to generate hash

--- block.js
+++ block.js
@@ -1,3 +1,5 @@
+const SHA256 = require('crypto-js/sha256');
+
 class Block {
 
   constructor(timestamp, lastHash, hash, data) {
@@ -9,12 +11,12 @@ class Block {
 
   toString() {
     return `
-          Block:
-           - timestamp = ${this.timestamp}
-           - lastHash = ${this.lastHash.substring(0, 10)}
-           - hash = ${this.hash.substring(0, 10)}
-           - data = ${this.data}
-        `;
+      Block:
+        - timestamp = ${this.timestamp}
+        - lastHash = ${this.lastHash.substring(0, 10)}
+        - hash = ${this.hash.substring(0, 10)}
+        - data = ${this.data}
+    `;
   }
 
   static genesis() {
@@ -22,12 +24,17 @@ class Block {
   }
 
   static mineBlock(lastBlock, data) {
-    const timestamp = Date.Now();
+    const timestamp = Date.now();
     const lastHash = lastBlock.hash;
-    const hash = 'hash-pending';
+    const hash = Block.hash(timestamp, lastHash, data);
 
     return new this(timestamp, lastHash, hash, data);
   }
+
+  static hash(timestamp, lastHash, data) {
+    return SHA256(`${timestamp}${lastHash}${data}`).toString();
+  }
+
 }
 
 module.exports = Block;
