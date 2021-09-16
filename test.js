af44d3edc6f1e30af7f6c2cc83c6dffc04ea26f4
Marilzon <maxmaril@hotmail.com>
2021-09-15 21:23:36 -0300

    Delete: Removing block tests

--- test.js
+++
@@ -1,8 +0,0 @@
-const Block = require('./block.js');
-
-const block = new Block('1234', '4568EAEA', 'HB20', '100');
-console.log(block.toString());
-console.log(Block.genesis().toString());
-
-const firstBlock = Block.mineBlock(Block.genesis(), '$500');
-console.log(firstBlock.toString());
