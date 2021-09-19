4064d113da2f6071b76eb0ab88a1ded670a51743
Marilzon <maxmaril@hotmail.com>
2021-09-19 16:27:41 -0300

    Update: unit test in currupt chain

--- blockchain.test.js
+++ blockchain.test.js
@@ -10,21 +10,30 @@ describe('Blockchain', () => {
     bc2 = new Blockchain;
   });
 
-  it('Starts  with genesis block', () => {
+  it('start with genesis block', () => {
     expect(bc.chain[0]).toEqual(Block.genesis());
   });
 
-  it('Adds a new block', () => {
+  it('add a new block', () => {
     const data = 'file.pdf';
     bc.addBlock(data);
 
     expect(bc.chain[bc.chain.length - 1].data).toEqual(data);
   });
 
-  it('Validates a valid chain', () => {
+  it('validates a valid chain', () => {
     bc2.addBlock('500 USD');
     expect(bc.isValidChain(bc2.chain)).toBe(true);
+  });
 
+  it('check a currupt chain in first block', () => {
+    bc2.chain[0].data = '0 USD';
+    expect(bc.isValidChain(bc2.chain)).toBe(false);
+  });
 
+  it('check a currupt chain', () => {
+    bc2.addBlock('200 USD');
+    bc2.chain[1].data = '0 USD';
+    expect(bc.isValidChain(bc2.chain)).toBe(false);
   });
 });
